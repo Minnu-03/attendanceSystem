f4a69dd87d210d2b57ff0a0a962292406d817571
Minnu-03 <105913116+Minnu-03@users.noreply.github.com>
2024-12-26 13:17:32 +0530

    Delete QR_system directory

--- QR_system/wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QR_Attendance_System.settings')
-
-application = get_wsgi_application()
